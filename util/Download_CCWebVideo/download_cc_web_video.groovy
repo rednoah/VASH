263adab750a4f9ae30e7cc4618459750803da07a
rednoah <reinhard.pointner@gmail.com>
2014-01-05 19:56:42 +0800

    * groovy script for easily grabbing ccwebvideo data on remote server

---
+++ util/Download_CCWebVideo/download_cc_web_video.groovy
@@ -0,0 +1,9 @@
+new File('Video_List.txt').splitEachLine('\t', 'UTF-8') { line ->
+	def queryId = line[1]
+	def filename = line[3]
+	
+	def file = new File("$queryId/$filename")
+	def url = new URL("http://vireo.cs.cityu.edu.hk/webvideo/videos/$queryId/$filename")
+	
+	println url.saveAs(file)
+}
