48ef4e1846b4820b318fe66f77c070667e6303cd
rednoah <reinhard.pointner@gmail.com>
2014-01-06 11:16:11 +0800

    * update

--- util/Download_CCWebVideo/download_cc_web_video.groovy
+++ util/Download_CCWebVideo/download_cc_web_video.groovy
@@ -4,6 +4,11 @@ new File('Video_List.txt').splitEachLine('\t', 'UTF-8') { line ->
 	
 	def file = new File("$queryId/$filename")
 	def url = new URL("http://vireo.cs.cityu.edu.hk/webvideo/videos/$queryId/$filename")
+
 	
-	println url.saveAs(file)
+	if (file.exists()) {
+		println "[SKIPPED] $file"
+	} else {
+		println url.saveAs(file)
+	}
 }
